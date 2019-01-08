c5737bdc2dd1910de24f91bc74fa499bf6c67312
Stuart Wilson <tuzgai@gmail.com>
2019-01-08 14:59:27 -0600

    Add markdown support

--- app/__init__.py
+++ app/__init__.py
@@ -3,12 +3,14 @@ from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_misaka import Misaka
 from config import Config
 
 bootstrap = Bootstrap()
 login = LoginManager()
 db = SQLAlchemy()
 migrate = Migrate()
+md = Misaka()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -18,6 +20,7 @@ def create_app(config_class=Config):
     login.init_app(app)
     db.init_app(app)
     migrate.init_app(app, db)
+    md.init_app(app)
     
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
