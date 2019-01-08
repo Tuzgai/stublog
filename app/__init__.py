8a4bea71dc81a6a3236db225329c0e1eba6a900f
Stuart Wilson <tuzgai@gmail.com>
2019-01-08 13:19:56 -0600

    Add database

--- app/__init__.py
+++ app/__init__.py
@@ -1,18 +1,27 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from config import Config
 
 bootstrap = Bootstrap()
+login = LoginManager()
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
     
     bootstrap.init_app(app)
+    login.init_app(app)
+    db.init_app(app)
+    migrate.init_app(app, db)
     
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
     
     return app
 
-from app import models
+from app import models, db
