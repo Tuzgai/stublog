1e11d76017bc56327b8be7ea468589bee197eef6
Stuart Wilson <tuzgai@gmail.com>
2019-01-09 13:21:51 -0600

    Add time formatting

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_misaka import Misaka
+from flask_moment import Moment
 from config import Config
 
 bootstrap = Bootstrap()
@@ -11,6 +12,7 @@ login = LoginManager()
 db = SQLAlchemy()
 migrate = Migrate()
 md = Misaka(fenced_code=True, hard_wrap=True, smartypants=True)
+moment=Moment()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
@@ -21,6 +23,7 @@ def create_app(config_class=Config):
     db.init_app(app)
     migrate.init_app(app, db)
     md.init_app(app)
+    moment.init_app(app)
     
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
