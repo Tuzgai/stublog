4abbc778dd9b77ffce233c8ba8be22b768100f7a
Stuart Wilson <tuzgai@gmail.com>
2019-01-09 11:04:41 -0600

    Add linking to posts

--- app/main/forms.py
+++ app/main/forms.py
@@ -3,5 +3,6 @@ from wtforms import StringField, TextAreaField, SubmitField
 from wtforms.validators import ValidationError, DataRequired
 
 class SubmitPostForm(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
     body = TextAreaField('Body', validators=[DataRequired()])
     submit = SubmitField('Submit')
--- app/main/routes.py
+++ app/main/routes.py
@@ -7,19 +7,24 @@ from app.models import User, Post
 from app.main.forms import SubmitPostForm
 
 @bp.route('/')
-@bp.route('/index.html')
+@bp.route('/index')
 def index():
     posts = Post.query.order_by(Post.timestamp.desc()).all()
     return render_template('index.html', posts=posts)
 
-@bp.route('/submit_post.html', methods=['GET', 'POST'])
+@bp.route('/submit_post', methods=['GET', 'POST'])
 @login_required
 def submitPost():
     form = SubmitPostForm()
     if form.validate_on_submit():
-        post = Post(body=form.body.data, author=current_user)
+        post = Post(title=form.title.data, body=form.body.data, author=current_user)
         db.session.add(post)
         db.session.commit()
         flash('Your post is live!', 'success')
         return redirect(url_for('main.index'))
     return render_template('submit_post.html', title="Submit Post", form=form)
+
+@bp.route('/post/<post>')
+def viewPost(post):
+    post = Post.query.filter_by(id=post).first()
+    return render_template('post.html', post=post)
