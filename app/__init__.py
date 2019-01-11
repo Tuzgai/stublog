23d9cb26a9669fb469a09f6fb35c29793612eb51
Stuart Wilson <tuzgai@gmail.com>
2019-01-11 16:11:00 -0600

    Fixed typo, removed unnecessary requirements

--- app/__init__.py
+++ app/__init__.py
@@ -64,7 +64,7 @@ def create_app(config_class=Config):
         
         if app.config['LOG_TO_STDOUT']:
             stream_handler = logging.StreamHandler()
-            stream_handler = setLevel(logging.INFO)
+            stream_handler.setLevel(logging.INFO)
             app.logger.addHandler(stream_handler)
         else:        
             if not os.path.exists('logs'):
