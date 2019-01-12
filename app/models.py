5c00e9381fc92b5276ca10f110507edbaafa72c2
Stuart Wilson <tuzgai@gmail.com>
2019-01-12 11:10:13 -0600

    Increase allowed email size

--- app/models.py
+++ app/models.py
@@ -12,7 +12,7 @@ from app import db, login
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True)
-    email = db.Column(db.String(12), index=True, unique=True)
+    email = db.Column(db.String(64), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     regdate = db.Column(db.DateTime, index=True, default=datetime.utcnow)
